5e71cc66f655cb2f7e8a444f3c945f3b26136f0b
David <ycool37@naver.com>
2023-11-12 00:15:46 +0900

    :new: 플레이어 애니메이션 추가

--- Assets/Undead Survivor/Code/Player.cs
+++ Assets/Undead Survivor/Code/Player.cs
@@ -9,11 +9,20 @@ public class Player : MonoBehaviour
     public float speed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
     }
 
     private void FixedUpdate()
@@ -22,6 +31,15 @@ public class Player : MonoBehaviour
         Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
 
+    }
+
+    private void LateUpdate()
+    {
+        anim.SetFloat("Speed",inputVec.magnitude);
+
+        if (inputVec.x != 0) {
+            spriter.flipX = inputVec.x < 0; 
+        }
     }
 
     void OnMove(InputValue value)
